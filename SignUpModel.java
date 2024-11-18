e9f1a8b7ca7c03a93e4d1958e9b2c438871a40a5
akishajnl <143833909+akishajnl@users.noreply.github.com>
2024-11-18 15:23:56 +0800

    SignUpModel.java

--- SignUpModel.java
+++ SignUpModel.java
@@ -11,36 +11,39 @@ public class SignUpModel {
                role != null && !role.equals("Option");
     }
 
-    // Getters and Setters
-    public String getIdNumber() {
-        return idNumber;
-    }
-
     public void setIdNumber(String idNumber) {
         this.idNumber = idNumber;
-    }
-
-    public String getPassword() {
-        return password;
     }
 
     public void setPassword(String password) {
         this.password = password;
     }
 
-    public String getDepartment() {
-        return department;
-    }
-
     public void setDepartment(String department) {
         this.department = department;
-    }
-
-    public String getRole() {
-        return role;
     }
 
     public void setRole(String role) {
         this.role = role;
     }
+    
+    public String getIdNumber() {
+        return idNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+ 
+    public String getDepartment() {
+        return department;
+    }
+
+
+    public String getRole() {
+        return role;
+    }
+
+ 
 }
